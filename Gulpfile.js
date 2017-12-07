743605064e9788d59f1296bc1227d5bc4c0cf3b8
sirkapil <kapil.math.hons@gmail.com>
2017-12-08 00:26:37 +0530

    Mr Hyde was here …

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,72 +1,152 @@
-var gulp = require('gulp'),
+var gulp = require('gulp');
 
-    gutil = require('gulp-util'),
+var browserSync = require('browser-sync');
 
-    sass = require('gulp-sass'),
+var less = require('gulp-less');
 
-    autoprefixer = require('gulp-autoprefixer'),
+var autoprefixer = require('gulp-autoprefixer');
 
-    minifycss = require('gulp-minify-css'),
+var minifyCSS = require('gulp-minify-css');
 
-    rename = require('gulp-rename'),
+var notify = require('gulp-notify');
 
-    shell = require('gulp-shell'),
+var gutil = require('gulp-util');
 
-    livereload = require('gulp-livereload'),
+var cp = require('child_process');
 
-    lr = require('tiny-lr'),
+var path = require('path');
 
-    server = lr();
+var uglify = require('gulp-uglify');
+
+var imagemin = require('gulp-imagemin');
+
+var pngquant = require('imagemin-pngquant');
 
 
-gulp.task('sass', function() {
 
-  return gulp.src('sass/style.scss')
+var messages = {
 
-    .pipe(sass())
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 
-    .pipe(autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ))
+};
 
-    .pipe(minifycss())
 
-    .pipe(rename('style.min.css'))
+/**
 
-    .pipe(gulp.dest('public/css'))
+* Build the Jekyll Site
 
-    .pipe(gulp.dest('_site/public/css'))
+*/
 
-    .pipe(livereload(server));
+gulp.task('jekyll-build', function (done) {
+
+    browserSync.notify(messages.jekyllBuild);
+
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+
+        .on('close', done);
+
+});
+
+
+/**
+
+* Rebuild Jekyll & do page reload
+
+*/
+
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+
+    browserSync.reload();
 
 });
 
 
-gulp.task('build', shell.task([ 'jekyll build' ]));
+/**
 
+* Wait for jekyll-build, then launch the Server
 
-gulp.task('watch', function() {
+*/
 
-  server.listen(35729, function(err) {
+gulp.task('browser-sync', ['less', 'jekyll-build'], function() {
 
-    if (err)
+    browserSync({
 
-      return console.log(err);
+        server: {
 
+            baseDir: '_site'
 
-    gulp.watch('sass/**/*.scss', ['sass']);
+        }
 
-
-    gulp.watch([
-
-      '_includes/**/*.html',
-
-      '_layouts/**/*.html',
-
-      '_posts/**/*'
-
-    ], ['build']);
-
-
-  });
+    });
 
 });
+
+
+// minifiy js
+
+gulp.task('compress', function() {
+
+  gulp.src('js/*.js')
+
+    .pipe(uglify())
+
+    .pipe(gulp.dest('js/min'))
+
+});
+
+
+// minifiy images
+
+gulp.task('images', function () {
+
+    return gulp.src('images/*')
+
+        .pipe(imagemin({
+
+            progressive: true,
+
+            svgoPlugins: [{removeViewBox: false}],
+
+            use: [pngquant()]
+
+        }))
+
+        .pipe(gulp.dest('images'));
+
+});
+
+
+gulp.task('less', function () {
+
+  gulp.src('_less/main.less')
+
+    .pipe(less({
+
+      paths: [ path.join(__dirname, '_less', 'includes') ]
+
+    }))
+
+    .pipe(less({compress: true}).on('error', gutil.log))
+
+    .pipe(autoprefixer('last 10 versions', 'ie 9'))
+
+    .pipe(minifyCSS({keepBreaks: false}))
+
+    .pipe(gulp.dest('css'))
+
+    .pipe(notify('Less Compiled, Prefixed and Minified'));
+
+});
+
+
+gulp.task('watch', function () {
+
+    gulp.watch('_less/**/*.less', ['less']);
+
+    gulp.watch(['index.html', '_layouts/*.html', '_posts/*.md', 'articles/*.html', 'profile/*.html', 'work/*.html', '_less/**/*.less', 'js/**/*', 'images/*'], ['jekyll-rebuild']);
+
+});
+
+
+gulp.task('default', ['browser-sync', 'watch']);
 
